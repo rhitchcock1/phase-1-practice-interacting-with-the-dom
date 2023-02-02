928ae1fdf778b421619158f142fcc5aceabd4fb4
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-02-02 11:46:18 -0500

    figuring out counter

--- js/challenge.js
+++ js/challenge.js
@@ -5,5 +5,28 @@ const heartBtn = document.querySelector("#heart")
 const likeBtn = document.querySelector("#like")
 
 const pauseBtn = document.querySelector("#pause")
-
+// let counter = document.querySelector("#counter")
 const minusBtn = document.querySelector("#minus")
+
+
+
+// let likes = plusBtn + 1
+const plusBtn = document.querySelector("#plus")
+plusBtn.addEventListener("click", (e) => {
+    counter = counter.id
+    counter.append( )
+    
+    let counter = document.querySelector("#counter")   
+    
+    
+    // counter.id = 1
+    // let counter = document.querySelector("#counter").value
+    // // .e.target.counter ++
+    console.log(counter)
+
+
+
+
+
+}) 
+
